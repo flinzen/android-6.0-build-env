c9635f4a2dbaa1635b4028e23f39a50dcc0945de
Dmitry Shchapin <diman199806@gmail.com>
2022-10-04 17:15:59 +0500

    fixed script

--- run-build.sh
+++ run-build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ccache_dir="$(pwd)/ccache"
-proj_dir="$(cd .. && pwd)"
-# sudo docker run --rm -ti -v "${ccache_dir}:/tmp/ccache" -v "${proj_dir}:/aosp" kylemanna/aosp:6.0-marshmallow bash
-# sudo docker run --rm -ti -v "${ccache_dir}:/tmp/ccache" -v "${proj_dir}:/aosp" kylemanna/aosp:latest bash
+proj_dir="$(cd android && pwd)"
+sudo docker build -t ayufan_14.04:latest ayufan_14.04/.
 sudo docker run --rm -ti -v "${ccache_dir}:/tmp/ccache" -v "${proj_dir}:/aosp" ayufan_14.04:latest /bin/bash
